a23abc7ce5ed8e576fb16df54f5b7c3f3b106216
Allan Pedro <paraestudo325@gmail.com>
2026-03-02 18:01:51 -0300

    added SecurityFilter class for token validation

--- api/src/main/java/br/com/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/br/com/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import br.com.med.voll.api.user.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +24,20 @@ public class TokenService {
                     .withExpiresAt(expirationDate())
                     .sign(algorithm);
         } catch (JWTCreationException exception){
-            throw new RuntimeException("erro ao gerrar token jwt", exception);
+            throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256("12345678");
+            return JWT.require(algoritmo)
+                    .withIssuer("API Medical_clinic")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
